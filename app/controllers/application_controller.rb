337b55edea2eec532145a6bee36c08b1550d9c87
Brice Roduit <brice.roduit@gmail.com>
2022-11-29 10:04:43 +0100

    modifications to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :authenticate_user!, only: :home
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :description, :adress, :zip_code, :city, :website])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :description, :adress, :zip_code, :city, :website])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website])
   end
-
-  def home
-  end
-
 end
