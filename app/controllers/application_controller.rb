bdbc11d7ce6c286c304662a1741ebb27c3978b0d
Brice Roduit <brice.roduit@gmail.com>
2022-11-30 23:01:03 +0100

    added user profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name, :descriptions, :adress, :zip_code, :city, :website, :avatar])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   # Defines the root path route ("/")
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :projects, only: %i[new create index]
+  resources :users, only: %i[index show]
 end
