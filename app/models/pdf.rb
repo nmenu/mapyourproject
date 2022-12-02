b96cf4aa9e17f0e318397f0c2d494af5b6877e7b
Brice Roduit <brice.roduit@gmail.com>
2022-12-02 07:46:02 +0100

    Can Upload pdfs and images

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,10 +1,13 @@
 class Pdf < ApplicationRecord
   belongs_to :project
   has_one_attached :pdf_file
-  validate :validate_attachment_filetypes
+  validate :correct_pdf_file_mime_type
 
+  private
 
-  def :validate_attachment_filetypes
-
+  def correct_pdf_file_mime_type
+    if pdf_file.attached? && !pdf_file.content_type.in?(%w(application/msword application/pdf))
+      errors.add(:pdf_file, 'Must be a PDF or a DOC file')
+    end
   end
 end
