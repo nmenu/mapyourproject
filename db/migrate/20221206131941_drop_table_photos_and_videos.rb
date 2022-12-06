80d813880d578285bf99d73581b7a65017730277
nicolas menu <nicolas.menu@wild-lights.com>
2022-12-06 14:21:25 +0100

    drop useless tables (for real this time)

--- db/migrate/20221206131941_drop_table_photos_and_videos.rb
+++ db/migrate/20221206131941_drop_table_photos_and_videos.rb
@@ -1,5 +1,4 @@
-class RemoveTablePhotoVideo < ActiveRecord::Migration[7.0]
-
+class DropTablePhotosAndVideos < ActiveRecord::Migration[7.0]
   def change
     drop_table :photos
     drop_table :videos
