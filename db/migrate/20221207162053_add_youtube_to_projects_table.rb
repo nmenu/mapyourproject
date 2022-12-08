2e92353fe940b92b1ba24ea70822757c0e2b9a14
nicolas menu <nicolas.menu@wild-lights.com>
2022-12-08 08:55:10 +0100

    new fields in the form

---
+++ db/migrate/20221207162053_add_youtube_to_projects_table.rb
@@ -0,0 +1,5 @@
+class AddYoutubeToProjectsTable < ActiveRecord::Migration[7.0]
+    def change
+      add_column :projects, :youtube, :string
+    end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_131941) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_162053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_131941) do
     t.string "details_02"
     t.string "details_03"
     t.string "details_04"
+    t.string "youtube"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
